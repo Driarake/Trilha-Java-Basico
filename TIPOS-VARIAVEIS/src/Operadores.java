0e16b65f766115a7080b2f8c8edd703bdbaa9ab8
Driarake <adriana_drix@yahoo.com.br>
2022-10-10 20:26:50 -0300

    operadores

---
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -0,0 +1,120 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA"; // concatenacao palavras.
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+        // UNÁRIOS
+
+        int numero = 5;
+
+        numero = -numero;
+        System.out.println(numero);
+
+        numero = +numero;
+        // o "+" é um operador aritmetico por isso continuou como -5 a resposta.
+        System.out.println(numero);
+
+        numero = numero * -1;
+        // para tornar positivo tem que multiplicar.
+        System.out.println(numero);
+
+        int numero1 = 5;
+        // x repeticao
+        // numero1 = numero1 + 1;
+        // numero1++;// mesma coisa que a forma descrita acima.
+
+        System.out.println(numero1++); // incrementar
+        System.out.println(++numero1); // incrementar
+        System.out.println(numero--); // decrementar
+        System.out.println(numero1);
+
+        boolean variavel = true; // pode ser true/false.
+
+        System.out.println(!variavel); // se torna "false".
+
+        variavel = !variavel; // forma de inverter uma variavel booleana.
+        System.out.println(variavel); // se torna de novo "true".
+
+        // TERNÁRIO
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "VERDADEIRA" : "FALSO"; // a mesma que o exemplo abaixo, porém menor.
+
+        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if (a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        int resultado1 = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+        System.out.println(resultado1);
+
+        // RELACIONAIS
+
+        String nomeUm = "Adriana";
+        String nomeDois = new String("Adriana");
+
+        System.out.println(nomeUm.equals(nomeDois));// equals compara conteúdos, usados para objetos.
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2) {// auxilia tomadas de decisão e consequentemente realizam fluxos na nossa
+            // aplicação dependendo se a expressão for "true".
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numero1 é igual a numero2?" + " " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero1 é diferente de numero2?" + " " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero1 é maior que numero2?" + " " + simNao);
+
+        // LÓGICOS
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if (condicao1 && (7 > 4)) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("fim");
+
+    }
+}
--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a clase String que representa texto na aplicação
+
+        String meuNome = "ADRIANA MARUMO"; // representar texto.
+
+        double salarioMinimo = 2500; // representar numero duplo.
+
+        short numeroCurto = 1; // representar numero pequeno.
+        int numeroNormal = numeroCurto; // representar valor inteiro.
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        // QUANDO TEM A PALAVRA "FINAL" NÃO POSSO MAIS MUDAR O VALOR.
+        final double VALOR_DE_PI = 3.14; // representar numero constante.
+        // VALOR_DE_PI = 10.75;
     }
 }
